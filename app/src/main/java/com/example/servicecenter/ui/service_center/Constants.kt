9399ca752040a8d70124fa2d3e66de94512b89f5
f-cet <f-cetiner99@hotmail.com>
2025-08-15 21:27:00 +0300

    WebView app first commit

---
+++ app/src/main/java/com/example/servicecenter/ui/service_center/Constants.kt
@@ -0,0 +1,5 @@
+package com.example.servicecenter.ui.service_center
+
+object Constants {
+    const val SERVICE_CENTER_URL = "http://servicecenter.utarit.com.tr/frmLogon.aspx?ReturnUrl=%2f"
+}
---
+++ app/src/main/java/com/example/servicecenter/ui/service_center/ServiceCenterFragment.kt
@@ -0,0 +1,71 @@
+package com.example.servicecenter.ui.service_center
+
+import android.annotation.SuppressLint
+import android.graphics.Bitmap
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.fragment.app.Fragment
+import com.example.servicecenter.databinding.FragmentServiceCenterBinding
+import com.example.servicecenter.ui.service_center.Constants.SERVICE_CENTER_URL
+
+class ServiceCenterFragment : Fragment() {
+
+    private var _binding: FragmentServiceCenterBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentServiceCenterBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    @SuppressLint("SetJavaScriptEnabled")
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setupWebView()
+        loadServiceCenter()
+    }
+
+    @SuppressLint("SetJavaScriptEnabled")
+    private fun setupWebView() = with(binding.serviceCenter) {
+        settings.apply {
+            javaScriptEnabled = true
+            loadsImagesAutomatically = true
+            /*
+            useWideViewPort = true  // Responsive olmayan sayfalarda tüm sayfayı ekrana sığdırmak için kullanılabilir.
+             */
+            loadWithOverviewMode = true
+            builtInZoomControls = true
+            displayZoomControls = false
+        }
+        webViewClient = object : WebViewClient() {
+            override fun onPageStarted(view: WebView?, url: String?, favicon: Bitmap?) {
+                progressBarVisibility(true)
+            }
+
+            override fun onPageFinished(view: WebView?, url: String?) {
+                progressBarVisibility(false)
+            }
+        }
+    }
+
+    private fun progressBarVisibility(isLoading: Boolean) {
+        binding.progressBar.visibility = if (isLoading) View.VISIBLE else View.GONE
+    }
+
+    private fun loadServiceCenter() {
+        binding.serviceCenter.loadUrl(SERVICE_CENTER_URL)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
